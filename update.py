4925fe37f85da9dc97c812c36e756e930a3d7ebf
thehappyguy56 <88908449+thehappyguy56@users.noreply.github.com>
2021-12-28 15:19:55 +0600

    Add files via upload

--- update.py
+++ update.py
@@ -37,17 +37,18 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = None
 
-if os.path.exists('.git'):
-    subprocess.run(["rm", "-rf", ".git"])
+if UPSTREAM_REPO is not None:
+    if os.path.exists('.git'):
+        subprocess.run(["rm", "-rf", ".git"])
 
-subprocess.run([f"git init -q \
-                  && git config --global user.email e.anastayyar@gmail.com \
-                  && git config --global user.name mltb \
-                  && git add . \
-                  && git commit -sm update -q \
-                  && git remote add origin {UPSTREAM_REPO} \
-                  && git fetch origin -q \
-                  && git reset --hard origin/master -q"], shell=True)
+    subprocess.run([f"git init -q \
+                      && git config --global user.email e.anastayyar@gmail.com \
+                      && git config --global user.name mltb \
+                      && git add . \
+                      && git commit -sm update -q \
+                      && git remote add origin {UPSTREAM_REPO} \
+                      && git fetch origin -q \
+                      && git reset --hard origin/master -q"], shell=True)
 
